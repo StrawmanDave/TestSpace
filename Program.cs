309e5bf7bab49efbc3541c1bb6710bde444c5ea6
StrawmanDave <davidgb.strawman@gmail.com>
2024-09-22 19:39:12 -0700

    The four sisters and the Duckbear

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
-﻿string name;
-Console.Write("What is your name?");
-name = Console.ReadLine();
+﻿Console.WriteLine("How many chocolate eggs where gathers?");
+string textChoco = Console.ReadLine();
 
-Console.Write("What is your age?");
-string ageText = Console.ReadLine();
-int age = Convert.ToByte(ageText);
-Console.WriteLine($"You are {name} and {age} years old");
+int numChoco = Convert.ToInt32(textChoco); // converts the input into a number not a string
+
+int sistersEggs; // the int variable must be declared first
+sistersEggs = numChoco / 4; // takes the input number and divides it by the 4 sisters
+Console.WriteLine($"Each sister gets {sistersEggs} chocolate eggs");
+
+int duckBearShare; // the int variable must be declared before assinging a value
+duckBearShare = numChoco  % 4; // takes the input number and finds the remainder.
+Console.WriteLine($"Duckbear gets {duckBearShare}");
+
+/* The 3 total egg counts where Duckbear gets more chocolate eggs are 6, 7, and 11. */
